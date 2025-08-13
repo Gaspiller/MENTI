ca200d8294a5204216b93c11c92eeb0c08d795f9
Gaspiller <2574664155@qq.com.>
2025-08-13 12:53:38 +0100

    update run_menti.sh

--- run_menti.sh
+++ run_menti.sh
@@ -1,10 +1,20 @@
-for i in $(seq 0 100); do
-    python MENTI.py \
-        --test \
-        --llm_model gpt-4 \
-        --embedding_model m3e \
-        --eval_index $i \
-        --case_path "./CalcQA/clinical_case.json" \
-        --tool_scale_path "./CalcQA/tool_scale.json" \
-        --tool_unit_path "./CalcQA/tool_unit.json"
-done
+# for i in $(seq 0 100); do
+#     python MENTI.py \
+#         --test \
+#         --llm_model gpt-4 \
+#         --embedding_model m3e \
+#         --eval_index $i \
+#         --case_path "./CalcQA/clinical_case.json" \
+#         --tool_scale_path "./CalcQA/tool_scale.json" \
+#         --tool_unit_path "./CalcQA/tool_unit.json"
+# done
+
+
+python MENTI.py \
+    --test \
+    --llm_model gpt-3.5 \
+    --embedding_model m3e \
+    --eval_index 56 \
+    --case_path "./CalcQA/clinical_case.json" \
+    --tool_scale_path "./CalcQA/tool_scale.json" \
+    --tool_unit_path "./CalcQA/tool_unit.json"
